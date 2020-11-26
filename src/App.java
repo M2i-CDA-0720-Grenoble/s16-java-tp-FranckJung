0a5d71db47b1dce975fc2a5c7bd110b2f70c29bb
Franck <franckjung@yahoo.fr>
2020-11-26 21:40:37 +0100

    repartition code dans classe game

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class App
 {
@@ -8,27 +7,11 @@ public class App
         System.out.print("\033[H\033[2J");   
         System.out.flush();
         
-        Scanner scanner = new Scanner(System.in);
-        String combinaison = DataProvider.createCombinaison();
-       
-        System.out.println(combinaison);
-
-        String UserInput = "";
-        System.out.println("Bienvenue dans le jeu du mastermind :");
-        System.out.println("Vous devez deviner la combinaison de 4 lettres (RVBJCM)");
-        System.out.println("Veuillez entrer une combinaison de 4 lettres");
-        while(!UserInput.equals(combinaison))
-        {
-            UserInput = scanner.nextLine().trim();
-            if(UserInput.length() == combinaison.length()){
-                if(!UserInput.equals(combinaison)){
-                    System.out.println("mauvaise combinaison");
-                }     
-            }else{
-                System.out.println("combinaison invalide");
-            }
+        Game game = new Game();
+        
+        while(game.getIsRunning()){
+            game.update();
         }
-        System.out.print("Vous avez gagné !");
-        scanner.close();
+      
     }
 }
