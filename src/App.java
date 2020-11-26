46e860b20a26fbda4cf45a2f86346c7a2c7c46c3
Franck <franckjung@yahoo.fr>
2020-11-26 20:54:47 +0100

    mise en place couleurs et combinaison aleatoire de lettre

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class App
 {
     // Cette méthode est appelée automatiquement au lancement de l'application
@@ -6,6 +10,46 @@ public class App
         System.out.print("\033[H\033[2J");   
         System.out.flush();
         
-        System.out.print("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int tailleCombinaison = 4;
+
+        //Genere un tableau de 4 chiffres aleatoire compris entre 0 et 5
+        int[] tableauNombresAleatoires = new int[tailleCombinaison];
+        for(int i = 0; i < tailleCombinaison; i++){
+            tableauNombresAleatoires[i] = random.nextInt(Couleur.values().length);
+        }
+        
+        //Genere un tableau de lettres correspondant aux couleurs des nombres du tableauNombresAleatoires
+        char[] tableauLettres = new char[tailleCombinaison];
+        for(int i = 0; i < tailleCombinaison; i++){
+            for (Couleur couleur : Couleur.values()) {
+                if (couleur.getCode() == tableauNombresAleatoires[i]) {
+                    tableauLettres[i] = couleur.getName();
+                }
+            }
+        }
+        
+        System.out.println(Arrays.toString(tableauNombresAleatoires));
+        System.out.println(Arrays.toString(tableauLettres));
+
+
+        // String UserInput = "";
+
+        // System.out.println("Veuillez entrer une combinaison de 4 lettres");
+        // while(!UserInput.equals(combinaison))
+        // {
+        //     UserInput = scanner.nextLine().trim();
+        //     if(UserInput.length() == combinaison.length()){
+        //         if(!UserInput.equals(combinaison)){
+        //             System.out.println("mauvaise combinaison");
+        //         }     
+        //     }else{
+        //         System.out.println("combinaison invalide");
+        //     }
+        // }
+        // System.out.print("Vous avez gagné !");
+        scanner.close();
     }
 }
