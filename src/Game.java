ff38a39947c9dda399866051ae89adf2b31cc1d3
Franck <franckjung@yahoo.fr>
2020-11-27 09:30:40 +0100

    ajout fonction rejouer

--- src/Game.java
+++ src/Game.java
@@ -5,34 +5,40 @@ public class Game
     private Scanner scanner;
     private Boolean isRunning;
     private String combinaison;
+    private String UserInput;
 
-    public Game() {
+    public Game()
+    {
         scanner = new Scanner(System.in);
         isRunning = true;
-        combinaison = DataProvider.createCombinaison();
+        UserInput ="";
     }
 
-    public void update(){
-        System.out.println(combinaison);
-
-        String UserInput = "";
-        System.out.println("Bienvenue dans le jeu du mastermind :");
-        System.out.println("Vous devez deviner la combinaison de 4 lettres (RVBJCM)");
-        System.out.println("Veuillez entrer une combinaison de 4 lettres");
-        while(!UserInput.equals(combinaison))
-        {
-            UserInput = scanner.nextLine().trim();
-            if(UserInput.length() == combinaison.length()){
-                if(!UserInput.equals(combinaison)){
-                    System.out.println("mauvaise combinaison");
-                }     
-            }else{
-                System.out.println("combinaison invalide");
+    public void update()
+    {
+        while(isRunning){
+            combinaison = DataProvider.createCombinaison();
+            System.out.println("Bienvenue dans le jeu du Mastermind !");
+            System.out.println("Vous devez deviner une combinaison de 4 couleurs (Rouge, Vert, Bleu, Jaune, Cyan et Magenta)");
+            System.out.println("Veuillez entrer une combinaison de 4 lettres :");
+            System.out.println("Reponse :" + combinaison);
+            System.out.print(">");
+    
+            while(!UserInput.equals(combinaison))
+            {
+                UserInput = scanner.nextLine().trim();
+                if(UserInput.length() == combinaison.length()){
+                    if(!UserInput.equals(combinaison)){
+                        System.out.println("mauvaise combinaison");
+                    }     
+                }else{
+                    System.out.println("combinaison invalide");
+                }
             }
+            rejouer();
         }
-        terminer();
     }
-
+        
     public Boolean getIsRunning() {
         return isRunning;
     }
@@ -41,7 +47,17 @@ public class Game
     {
         isRunning = false;
         scanner.close();
-        System.out.print("Vous avez gagné !");
+        System.out.print("Aurevoir !");
+    }
+
+    public void rejouer()
+    {
+        System.out.println("Vous avez gagné !");
+        System.out.println("Voulez vous rejouer ? (Y/N)");
+        UserInput = scanner.nextLine().trim();
+        if("N".equals(UserInput)){
+            terminer();
+        }
     }
 
 }
